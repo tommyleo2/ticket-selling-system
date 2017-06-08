17b5aa8e2b114e837e5749fc53d29dd93826018e
tommyleo <137581768@qq.com>
2017-06-09 03:35:15 +0800

    add CI

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,13 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 from app import app
 from app.utility.config import g_config
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{user}:{password}@{server}:3306/{database}?\
+if "ENV" in os.environ and os.environ["ENV"] == "CI_ENV":
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite3:///tmp.db"
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{user}:{password}@{server}:3306/{database}?\
 charset=utf8".format(user=g_config.get("MySQL", "username"),
                      password=g_config.get("MySQL", "password"),
                      server=g_config.get("MySQL", "server"),
                      database=g_config.get("MySQL", "database"))
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
